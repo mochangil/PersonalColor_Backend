5fb155729dab9574a73e3cb9c59edd4326f7f9d9
[mochangil] <ahckddlf1234@gmail.com>
2023-11-02 20:12:30 +0900

    Feat : Generated cookie with personal color id (cors, ML not done yet)

--- src/main/java/Mo/PersonalColorBackend/controller/FaceAnalyzeController.java
+++ src/main/java/Mo/PersonalColorBackend/controller/FaceAnalyzeController.java
@@ -1,22 +1,48 @@
 package Mo.PersonalColorBackend.controller;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.aspectj.bridge.Message;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/faceAnalyze")
 public class FaceAnalyzeController {
 
-    @PostMapping
-    public Long personalColorAnalyze(@RequestBody String img){
-        //analyze face info
-        Long id = Long.valueOf(1);
-        return (id);
+    //@PostMapping
+    @GetMapping
+    public ResponseEntity personalColorAnalyze( //@RequestBody String img,
+                                                HttpServletResponse response,
+                                                @CookieValue(value="personalColorId", required = false)Cookie cookie){
+        try {
+            //analyze face info, find personalColor.
+            String id = "1";
+            id = URLEncoder.encode(id, "utf-8");
+            Cookie newCookie = new Cookie("personalColor",id);
+            newCookie.setPath("/");
+            newCookie.setMaxAge(30);
+            response.addCookie(newCookie);
+            return new ResponseEntity(HttpStatus.OK);
+        }catch(UnsupportedEncodingException e){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
+
     }
 
 }
