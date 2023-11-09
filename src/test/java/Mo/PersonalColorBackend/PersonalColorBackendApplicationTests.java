a12949292c020ce251eba4c747a15dd4d7bb78bb
[mochangil] <ahckddlf1234@gmail.com>
2023-11-09 16:01:00 +0900

    Deploy test #2

--- src/test/java/Mo/PersonalColorBackend/PersonalColorBackendApplicationTests.java
+++ src/test/java/Mo/PersonalColorBackend/PersonalColorBackendApplicationTests.java
@@ -3,7 +3,6 @@ package Mo.PersonalColorBackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PersonalColorBackendApplicationTests {
 
 	@Test
