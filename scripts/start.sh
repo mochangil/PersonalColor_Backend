30078c369d91c1241477917bd90bbb8fb1496740
[mochangil] <ahckddlf1234@gmail.com>
2023-11-18 08:45:04 +0900

    Fix : ML

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,9 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+#echo "$TIME_NOW > python 패키지 설치" >> $DEPLOY_LOG
+#nohup pip install -r requirements.txt
+
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
@@ -17,3 +20,4 @@ nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
