83fa21b0ce84dbd09e112e622ee71c3e7a8d0fdf
[mochangil] <ahckddlf1234@gmail.com>
2023-11-24 15:52:14 +0900

    Feat : Add permission at python directory

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ubuntu/app" #코드가 주입되는 경로
 JAR_FILE="$PROJECT_ROOT/PersonalColorApp.jar" #build.gradle에서 설정한 파일명으로 변경
+SCRIPT_DIR="$PROJECT_ROOT/src/main/java/Mo/PersonalColorBackend/ML/personal"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -14,6 +15,8 @@ TIME_NOW=$(date +%c)
 
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+chmod 777 $SCRIPT_DIR
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
